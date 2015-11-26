353caca5ae422e7b74209dcd78b8bacc32da5541
jeanpokou <pokoujeanmarc@gmail.com>
2015-11-26 17:28:48 -0400

    cleaning api/Driver.js #1

--- app/api/Drivers.js
+++ app/api/Drivers.js
@@ -2,20 +2,32 @@
 var spawn = require('child_process').spawn;
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
+var dbPath = 't:/TDSM_Wagropur/dbf/';
+var apiExe = spawn(__dirname + '/dataLayer/dataRequestModule.exe',
+[]);
 
 var DriversApi = function() {
 
   var self = this;
+  // listener for spawn  data event
+  apiExe.stdout.on('data',function(data) {
+        console.log(data.toString('utf-8'));
+        self.emit('drivers',data.toString('utf-8'));
+      });
 
-  self.getDrivers = function() {
-
-    var test = spawn(__dirname + '/dataLayer/dataRequestModule.exe',
-    ['t:/TDSM_Wagropur/dbf/driver.dbf']);
-    test.stdout.on('data',function(data) {
-      self.emit('drivers',data.toString('utf-8'));
-    });
+  self.loadDrivers = function() {
+    apiExe.stdin.write(dbPath + 'driver.dbf\n');
   };
-  self.getDrivers();
+
+  self.edit = function() {
+    //  test.stdin.setEncoding('utf-8');
+    //  test.stdin.write('c:/tdsm_w/dbf/drivers.dbf');
+  };
+
+  self.on('edit',self.edit);
+
+  //loading the drives list
+  self.loadDrivers();
 };
 util.inherits(DriversApi,EventEmitter);
 module.exports = DriversApi ;
